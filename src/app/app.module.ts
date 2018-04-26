6faf4cb6232c53cd64b91720508c402887eb5ad7
Hardikkumar Patel <hardikkumar.patel.1@etsmtl.net>
2018-04-25 21:38:39 -0400

    Create contact list template & component

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactDetailsComponent,
+    ContactListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
